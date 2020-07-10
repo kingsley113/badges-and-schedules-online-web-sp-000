a01a4a5f2d2e9cafd2e911a4d432e5ee537fba48
Cameron Kingsley <cameron.kingsley13@gmail.com>
2020-07-10 19:42:24 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,23 @@
 # Write your code here.
+def badge_maker(name)
+  puts "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(names)
+  counter = 0
+  names.each do |name|
+    badges[counter] = badge_maker(name)
+    counter += 1
+  end
+
+
+
+def assign_rooms(names)
+
+end
+
+
+
+def printer()
+
+end
